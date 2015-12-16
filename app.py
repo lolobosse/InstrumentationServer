4f62f515b1eb8506636e04a6825e755f3449a2b0
Laurent Meyer <laurent@moid.de>
2015-12-16 13:42:13 +0100

    Merged the app.py for opening to external apps

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import json
+from django.shortcuts import redirect
 
 async_mode = None
 
 if async_mode is None:
     try:
         import eventlet
+
         async_mode = 'eventlet'
     except ImportError:
         pass
@@ -12,6 +15,7 @@ if async_mode is None:
     if async_mode is None:
         try:
             from gevent import monkey
+
             async_mode = 'gevent'
         except ImportError:
             pass
@@ -25,9 +29,11 @@ if async_mode is None:
 # thread
 if async_mode == 'eventlet':
     import eventlet
+
     eventlet.monkey_patch()
 elif async_mode == 'gevent':
     from gevent import monkey
+
     monkey.patch_all()
 
 import time
@@ -36,8 +42,9 @@ import subprocess
 from os import chdir
 import os
 import signal
+from uuid import uuid4
 
-from flask import Flask, render_template, send_file, request, session
+from flask import Flask, render_template, send_file, request, session, url_for
 from flask_socketio import SocketIO, emit
 
 import InstrumentationScripts as IS
@@ -53,6 +60,7 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 
 thread_map = {}
+objects_for_thread = {}
 
 
 def log(process, sid):
@@ -73,18 +81,25 @@ def background_thread(sid):
                   {'data': "Thread Started", 'count': 0},
                   namespace='/test')
     cb = IS.CommandBuilder()
+    # Replace the ini with the upload at runtime
+    if 'Apk' in objects_for_thread[sid].keys():
+        cb.apkFilePath = objects_for_thread[sid]['Apk']
+    if 'Source' in objects_for_thread[sid].keys():
+        cb.sourceFilePath = objects_for_thread[sid]['Source']
+    if 'Sink' in objects_for_thread[sid].keys():
+        cb.sinkFilePath = objects_for_thread[sid]['Sink']
+    if 'TaintWrapper' in objects_for_thread[sid].keys():
+        cb.taintWrapperPath = objects_for_thread[sid]['TaintWrapper']
     args = cb.createCommand().split()
     chdir(cb.InstrumentationPepDirectory)
     instrumentation = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     log(instrumentation, sid)
-    print(cb.createCommandSign())
     sign = subprocess.Popen(cb.createCommandSign().split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     log(sign, sid)
     socketio.emit('my response',
                   {'data': "Thread Finished", 'count': 0},
                   namespace='/test')
     thread_map[sid] = False
-
 
 
 @app.route('/')
@@ -97,10 +112,12 @@ def test_connect():
     print("Client connected")
     emit('my response', {'data': 'Connected<br>', 'count': 0})
 
+
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     thread_map[request.sid] = False
     print('Client disconnected')
+
 
 @socketio.on('startInstrumentation', namespace='/test')
 def test_start_stop(message):
@@ -118,10 +135,68 @@ def test_start_stop(message):
 def send_jquery():
     return send_file('bower_components/jquery/dist/jquery.min.js')
 
+
 @app.route('/socketIo')
 def send_socketio():
     return send_file('static/socketIo/socketio.js')
 
 
+def saveFile(target, upload):
+    filename = upload.filename.rsplit("/")[0]
+    destination = "/".join([target, filename])
+    print "Accept incoming file:", filename
+    print "Save it to:", destination
+    upload.save(destination)
+    return destination
+
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    form = request.form
+    # Create a unique "session ID" for this particular batch of uploads.
+    upload_key = str(uuid4())
+
+    # Is the upload using Ajax, or a direct POST by the form?
+    is_ajax = False
+    if form.get("__ajax", None) == "true":
+        is_ajax = True
+
+    # Target folder for these uploads.
+    target = '/'.join([os.path.dirname(os.path.realpath(__file__)), "static/uploads/{}".format(upload_key)])
+    try:
+        if not os.path.exists(target):
+            os.makedirs(target)
+    except OSError as e:
+        print e
+        if is_ajax:
+            return ajax_response(False, "Couldn't create upload directory: {}".format(target))
+        else:
+            return "Couldn't create upload directory: {}".format(target)
+    print(request.files.getlist("file"))
+    if request.files.has_key("Apk"):
+        upload = request.files.get("Apk")
+        objects_for_thread[form['sid']] = {'Apk': saveFile(target, upload)}
+        # print(objects_for_thread)
+    if request.files.has_key("Source"):
+        upload = request.files.get("Source")
+        objects_for_thread[form['sid']] = {'Source': saveFile(target, upload)}
+    if request.files.has_key("Sink"):
+        upload = request.files.get("Sink")
+        objects_for_thread[form['sid']] = {'Sink': saveFile(target, upload)}
+    if request.files.has_key("TaintWrapper"):
+        upload = request.files.get("TaintWrapper")
+        objects_for_thread[form['sid']] = {'TaintWrapper': saveFile(target, upload)}
+    return ajax_response(True, upload_key)
+
+
+
+def ajax_response(status, msg):
+    status_code = "ok" if status else "error"
+    return json.dumps(dict(
+        status=status_code,
+        msg=msg,
+    ))
+
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="0.0.0.0")
